3929baf680b11fe24f16f6a54542537a16099e38
chaitrapatil <pchaitra555@gmail.com>
2021-09-13 12:23:05 +0530

    Message changed in sum.java

--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public static void main(
 String[] args){
 int num1=5,num2 = 10,sum;
 sum=num1 + num2;
-System.out.println("sum of these numbers: "+sum);
+System.out.println("sum of these numbers is: "+sum);
 }
 }
